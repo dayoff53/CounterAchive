91a41a2d6eafdb017a1e0495dad964726a5b2cd7
dayoff53 <jaehyunlee53529@gmail.com>
2024-11-26 05:09:51 +0900

    feat : SkillEffect의 기능 일부를 StageManager로 이전

--- Assets/Script/Manager/StageManager/StageManager_Skill.cs
+++ Assets/Script/Manager/StageManager/StageManager_Skill.cs
@@ -5,12 +5,6 @@ using UnityEngine;
 
 public partial class StageManager
 {
-    private DataManager dataManager;
-
-    private void Start()
-    {
-        dataManager = DataManager.Instance;
-    }
 
     /// <summary>
     /// 스킬 슬롯을 초기화하고 주어진 스킬 데이터로 설정합니다.
@@ -27,21 +21,56 @@ public partial class StageManager
 
     public void SkillStart()
     {
+        SkillData skillData = currentSkillSlot.skillData;
         currentPrograssState = ProgressState.SkillPlay;
         //StartCoroutine(inGameManager.DelayTurnEnd(inGameManager.currentSkillSlot.skillData.skillEndTime));
         unitSlotList[currentTurnSlotNumber].unit.SetAnim(1);
-        cost -= currentSkillSlot.skillData.skillCost;
+        cost -= skillData.skillCost;
+
+
+        NormalAttack(skillData, unitSlotList[currentTurnSlotNumber].unit, unitSlotList[skillTargetNum].unit);
     }
 
     public void SkillHitPlay()
     {
         SkillData skillData = currentSkillSlot.skillData;
-        float skillDamage = skillData.damage;
 
         switch(skillData.skillTypeState)
         {
             case SkillTypeState.Attack:
-                skillDamage += (unitSlotList[currentTurnSlotNumber].unit.atk);
+                HitProduction();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 현 목표에게 공격을 수행합니다.
+    /// </summary>
+    /// <param name="damage">적용할 데미지입니다.</param>
+    public void ExecuteAttack(UnitBase target, float damage)
+    {
+        target.currentHp -= damage;
+    }
+
+    public virtual void HitProduction()
+    {
+        GameObject hitProductonObject = poolManager.Pop(currentSkillSlot.skillData.skilIHitProductionObject);
+        hitProductonObject.transform.position = unitSlotList[skillTargetNum].transform.position;
+    }
+
+
+    protected virtual void NormalAttack(SkillData skillData, UnitBase user, UnitBase target)
+    {
+        float skillDamage = skillData.damage;
+
+        switch (skillData.skillTypeState)
+        {
+            case SkillTypeState.Attack:
+                skillDamage += (user.atk);
+                ExecuteAttack(target, skillDamage);
                 break;
 
             default:
