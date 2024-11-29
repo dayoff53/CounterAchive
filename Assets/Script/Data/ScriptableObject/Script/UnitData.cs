bc8868ddc6778022653baa6559560c5fa46421cc
dayoff53 <jaehyunlee53529@gmail.com>
2024-11-29 15:05:31 +0900

    Move : Script/ScriptableObject 폴더의 위치를 Script/Data/ 안으로 이동

---
+++ Assets/Script/Data/ScriptableObject/Script/UnitData.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 유닛의 출신지 (필드에 따라 스텟의 차이가 발생한다)
+/// </summary>
+[System.Serializable]
+public enum UnitNative
+{
+    CounterSide,
+    BlueAchive,
+    Nikke
+}
+
+/// <summary>
+/// 유닛의 작품 중 역할 (파티 구성에 따라 스텟에 차이가 발생한다)
+/// </summary>
+[System.Serializable]
+public enum UnitRole
+{
+    Main,
+    Sub,
+    Mob
+}
+
+/// <summary>
+/// 수정 가능한 스텟의 State 값
+/// </summary>
+[System.Serializable]
+public enum PublicUnitStatusState
+{
+    maxHp,
+    currentHp,
+    maxAp,
+    atk,
+    def,
+    speed,
+    acc,
+    eva,
+    currentActionPoint
+}
+
+
+/// <summary>
+/// 유닛의 가장 기초가되는 원본 데이터. (추후 게임 플레이를 통해 유닛을 강화하거나 업그레이드 할 수는 있으나, 가장 기본적인 데이터는 해당 데이터를 기준으로 작업한다.)
+/// </summary>
+[CreateAssetMenu(fileName = "New UnitData", menuName = "Datas/UnitData")]
+public class UnitData : ScriptableObject
+{
+    [Header("Unit Stats")]
+    public int unitNumber = 0;
+    public string unitName;
+    public Sprite unitFaceIcon;
+    public Sprite unitSprite;
+    public RuntimeAnimatorController unitAnimController;
+    public Vector3 hitPosition;
+
+    public float hp = 10;
+    public float ap = 10;
+    public int atk = 1;
+    public int def = 1;
+    public int speed = 1;
+    public float acc = 1;
+    public float eva = 1;
+    public List<SkillData> skillDataList; 
+}
+ 
